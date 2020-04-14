a9ca4faf8b3ce80fd3634bf27115fa03dc9ec081
Kwatanwa17 <kwatanwa17@gmail.com>
2020-04-14 23:56:39 +0200

    Node MongoDB Example 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
@@ -11,25 +12,35 @@ MongoClient.connect(url, (err, client) => {
     console.log('connected correctly to server');
 
     const db = client.db(dbname);
-    const collection = db.collection('dbname');
 
-    collection.insertOne({"name": "Uthapizza", "description": "test"}, (err, result) => {
-        assert.equal(err, null);
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+        "dishes", (result) => {
+            console.log("Insert Document:\n", result.ops);
 
-        console.log('After insert:\n');
-        console.log(result.ops);
+            dboper.findDocuments(db, "dishes", (docs) => {
+                console.log("Found Documents:\n", docs);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err, null);
+                dboper.updateDocument(db,
+                    { name: "Vadonut"},
+                    {description: "Updated Test" },
+                    "dishes",
+                    (result) => {
+                        console.log("Updated Document:\n", result.result);
 
-            console.log('Found:\n');
-            console.log(docs);
+                        dboper.findDocuments(db, "dishes", (docs) => {
+                            console.log("Found Updated Documents:\n", docs);
 
-            db.dropCollection('dishes', (err, result) => {
-                assert.equal(err, null);
+                            db.dropCollection("dishes", (result) => {
+                                console.log("Dropped Collection: ", result);
 
-                client.close();
-            })
-        })
-    })
-})
+                                client.close();
+                            });
+                        });
+                    });
+            });
+        });
+
+
+
+
+});

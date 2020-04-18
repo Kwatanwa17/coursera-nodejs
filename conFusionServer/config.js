280530e03cd3f04b8d118f048a5554fca2d4a698
Kwatanwa17 <kwatanwa17@gmail.com>
2020-04-18 03:52:51 +0200

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '2132144323254325',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '258390101867293',
+        clientSecret: '45936833700ce0b18e80b3945cf74861'
+    }
 }
